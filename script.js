5393e48bca733f35c5372069fa328b1acaf6e70e
Degan90 <poorya.degan@gmail.com>
2021-06-10 19:38:48 -0400

    final

--- script.js
+++ script.js
@@ -3,15 +3,21 @@ const buttonHeros = ["slark","cm","tuskar","weaver"];
 const startButton = document.querySelector("#start")
 const levelDisplay =document.querySelector("#score-title")
 const replayButton = document.querySelector("#replay")
-const heroButtons = document.getElementsByClassName("container")
+const heroButtons = document.querySelectorAll(".btns")
 const extraSound = document.querySelector("#extra")
+const high = document.querySelector("#high")
 let gamePattern = [];
 let userClickedPattern = [];
+
+let highestLevel = 0;
+
 
 
 
 let level = 0 ;
 let started = false;
+
+
 const startGame = ()=>{
         levelDisplay.innerText = "Level    " + level;
         nextSequence();
@@ -29,7 +35,12 @@ const playSound = (name)=>{
 const userClicked = (event)=>{
     event.preventDefault();
     let userChosenHero = event.target.id;
-    userClickedPattern.push(userChosenHero); 
+    userClickedPattern.push(userChosenHero);
+    
+    if(userClickedPattern.length>highestLevel){
+        highestLevel = userClickedPattern.length ;
+
+    }
     playSound(userChosenHero)
     animatePress(userChosenHero)
     //Call checkAnswer() after a user has clicked and chosen their answer,
@@ -54,7 +65,9 @@ const checkAnswer = (currentLevel) =>{
         levelDisplay.innerText = "Game Over , Press Replay Button "
         document.querySelector("body").classList.add("game-over");
         startButton.disabled = true;
-        heroButtons.disabled = true;  
+        heroButtons.forEach(element => {
+            element.disabled = true;
+        }); 
     }   
 }
 
@@ -62,7 +75,10 @@ const nextSequence = () =>{
     userClickedPattern=[];
     level++;
     levelDisplay.innerText = "Level    " + level;
-    
+    console.log(level,highestLevel)
+    if(level>highestLevel){
+        high.innerText=level
+    }
     let randomNumber = Math.floor(Math.random()*4);
     let randomChosenHero = buttonHeros[randomNumber];
     
@@ -112,10 +128,14 @@ const animatePress = (x) =>{
 
 
 const startOver = () =>{
+//   highestLevel = userClickedPattern.length
   level = 0;
   gamePattern = [];
   document.querySelector("body").classList.remove("game-over");
   startButton.disabled = false;
+  heroButtons.forEach(element => {
+    element.disabled = false;
+});
   levelDisplay.innerText = ""
 //   playSound("music")
 
